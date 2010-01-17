e56bd432465d66d8865f4f5a7de549abbfe01372
Andrew Watts <andrewwatts@gmail.com>
2010-01-17 18:44:47 -0500

    initial commit of ui.tabs.closable
    An unobtrusive way to add a close button to jQuery UI tabs.

---
+++ README.md
@@ -0,0 +1,34 @@
+
+Closable Tabs for jQuery UI
+=============================
+
+An unobtrusive way to add a close button to jQuery UI tabs.
+
+Demo
+-----
+
+Demo of the close button functionality at 
+[http://andrew.io/playpen/ui.tabs.closable/demo/][demo]
+
+
+How To Use
+-----------
+
+0. Follow the instructions on the [jQuery UI Tabs][jQuery UI Tabs] site and 
+   include the necessary jQuery and jQuery UI files.  Then read through the
+   overview at the [jQuery UI Tabs][jQuery UI Tabs] site to get an idea of
+   what is possible. 
+1. Include the javascript file in your html file or js loader. eg:
+
+    `<script type="text/javascript" src="ui.tabs.closable.min.js"></script>`
+    
+2. add a closable option when instatiating the tabs:
+
+    `$('#tabs').tabs({closable: true})`
+    
+That's it, about as unobtrusive as I could make it.
+
+
+[demo]: http://andrew.io/playpen/ui.tabs.closable/demo/
+[jQuery UI Tabs]: http://jqueryui.com/demos/tabs/
+
---
+++ ui.tabs.closable.js
@@ -0,0 +1,62 @@
+/*!
+ * 
+ * Copyright (c) 2010 Andrew Watts
+ *
+ * Dual licensed under the MIT (MIT_LICENSE.txt)
+ * and GPL (GPL_LICENSE.txt) licenses
+ * 
+ * http://github.com/andrewwatts/
+ *
+ * Depends:
+ *     jquery-ui
+ * 
+ */
+(function() {
+    
+var ui_tabs_tabify = $.ui.tabs.prototype._tabify;
+
+$.extend($.ui.tabs.prototype, {
+
+    _tabify: function() {
+        var self = this;
+
+        ui_tabs_tabify.apply(this, arguments);
+
+        // if closable tabs are enable, add a close button
+        if (self.options.closable === true) {
+
+            var unclosable_lis = this.lis.filter(function() {
+                // return the lis that do not have a close button
+                return $('span.ui-icon-circle-close', this).length === 0;
+            });
+
+            // append the close button and associated events
+            unclosable_lis.each(function() {
+                $(this)
+                    .append('<a href="#"><span class="ui-icon ui-icon-circle-close"></span></a>')
+                    .find('a:last')
+                        .hover(
+                            function() {
+                                $(this).css('cursor', 'pointer');
+                            },
+                            function() {
+                                $(this).css('cursor', 'default');
+                            }
+                        )
+                        .click(function() {
+                            var index = self.lis.index($(this).parent());
+                            if (index > -1) {
+                                // remove this tab
+                                self.remove(index)
+                            }
+
+                            // don't follow the link
+                            return false;
+                        })
+                    .end();
+            });
+        }
+    }
+});
+    
+})(jQuery);
