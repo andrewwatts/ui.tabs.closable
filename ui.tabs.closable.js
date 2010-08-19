ef6ce304ad682d098003d3b6181e25e49b9678e3
Corey Olsen <corey@ryati.com>
2010-08-20 03:35:04 +0800

    Adding a callback for closing the tab
    Users of ui.tabs.closable may want the option of providing a callback
    function to determine whether or not it's right to close the tab based
    on an application's policies. If no call back is provided the
    functionality remains the same as before.

--- ui.tabs.closable.js
+++ ui.tabs.closable.js
@@ -41,6 +41,9 @@ $.extend($.ui.tabs.prototype, {
                         .click(function() {
                             var index = self.lis.index($(this).parent());
                             if (index > -1) {
+                                // call _trigger to see if remove is allowed
+                                if (false === self._trigger("closableClick", null, self._ui( $(self.lis[index]).find( "a" )[ 0 ], self.panels[index] ))) return;
+
                                 // remove this tab
                                 self.remove(index)
                             }
